edb67bbfd298cf9c91ff32ddb69dd7895817578c
ren4in <ren4in@gmail.com>
2020-03-19 16:12:36 +0300

    Fourth Task Fix

--- index.php
+++ index.php
@@ -10,7 +10,9 @@ $layout_content=include_template('layout.php', [
     'p_title' => 'Главная страница',
     'is_auth' => $is_auth,
     'user_name' => $user_name,
-    'page_content' => $page_content]);
+    'page_content' => $page_content,
+    'Categories_list' => $Categories_list,
+]);
 print($layout_content);
 
 ?>
